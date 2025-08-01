fb080f888e18e8ae452d3b5a820acfa3b21638fd
davsam97 <dava973@live.fr>
2025-08-01 14:09:56 +0200

    version avec main en commentaire

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samuel <samuel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 13:54:12 by samuel            #+#    #+#             */
+/*   Updated: 2025/08/01 14:08:36 by samuel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void);
+
+void	ft_print_alphabet(void)
+{
+	char	c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		write(1, &c, 1);
+		c++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}
+*/
