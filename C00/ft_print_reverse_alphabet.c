15c4273278d4a3b344c4c71abe947ad554068f5d
davsam97 <dava973@live.fr>
2025-08-01 14:16:58 +0200

    version avec main en commentaire

---
+++ C00/ft_print_reverse_alphabet.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samuel <samuel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 14:12:17 by samuel            #+#    #+#             */
+/*   Updated: 2025/08/01 14:14:56 by samuel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void);
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write(1, &c, 1);
+		c--;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
+*/
