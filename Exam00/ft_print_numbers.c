596e804b49e50fd0a4dd2174772917c9b5bbf808
davsam97 <dava973@live.fr>
2025-08-07 19:57:35 +0200

    use while loop and only write the function

---
+++ Exam00/ft_print_numbers.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: samuel <samuel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/07 19:49:33 by samuel            #+#    #+#             */
+/*   Updated: 2025/08/07 19:56:12 by samuel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_numbers(void);
+
+void	ft_print_numbers(void)
+{
+	int	number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		write(1, &number, 1);
+		number++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_numbers();
+	return (0);
+}
+*/
